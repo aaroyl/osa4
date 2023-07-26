38a263c63e7872cb57f91c4c63a602b4d5d711ec
Aaro Ylimaula <aaro.ylimaula@aalto.fi>
2023-07-26 12:30:14 +0300

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,13 +1,27 @@
 const dummy = (blogs) => {
-  return 1
+    return 1
 }
 
 const totalLikes = (blogs) => {
     return blogs.length === 0
     ? 0
-    : blogs.reduce((a, b) => a + b.likes, 0)
+    : blogs.reduce((a, blog) => a + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    if(blogs.length === 0){
+        return {}
+    }else{
+        let mostLikes = blogs.reduce((max, blog) => blog.likes > max ? blog.likes : max, blogs[0].likes)
+        for(let i = 0; i < blogs.length; i++){
+            if(blogs[i].likes === mostLikes){
+                return blogs[i]
+            }
+        }
+        return {}
+    }
 }
   
 module.exports = {
-  dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
