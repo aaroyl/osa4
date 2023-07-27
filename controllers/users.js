052f06b48872a24776e7305f7a4eb57c17235222
Aaro Ylimaula <aaro.ylimaula@aalto.fi>
2023-07-27 08:52:20 +0300

    4.16

--- controllers/users.js
+++ controllers/users.js
@@ -3,21 +3,30 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 const { response } = require('../app')
 
-usersRouter.post('/', async (request, response) => {
+usersRouter.post('/', async (request, response, next) => {
     const {username, name, password} = request.body
+
+    if(password.length <= 3){
+        return response.status(400).json({error: 'Too short password'})
+    }
 
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
-    const user = new User({
-        username, 
-        name, 
-        passwordHash
-    })
-
-    const savedUser = await user.save()
-
-    response.status(201).json(savedUser)
+    try{
+        const user = new User({
+            username, 
+            name, 
+            passwordHash
+        })
+    
+        const savedUser = await user.save()
+    
+        response.status(201).json(savedUser)
+    }catch(exception){
+        next(exception)
+    }
+    
 })
 
 usersRouter.get('/', async (request, response) => {
